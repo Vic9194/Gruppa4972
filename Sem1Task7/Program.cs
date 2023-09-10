3bc3f865d639abc726bb43e5173e4bdb9d4d61db
Victor <Wowhill9194@mail.ru>
2023-09-11 02:08:32 +0700

    list 2

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿﻿// Console.WriteLine("Введите число:");
+// int num = int.Parse(Console.ReadLine()??"0");
+
+// Console.WriteLine(num < 1000 && num > 99 ? num % 10 : "Число не трёхзначное");
+
+Console.WriteLine("Enter the number: ");
+int num=int.Parse(Console.ReadLine()??"0");
+
+if(num>99&&num<1000)
+{
+    int lastNum=num%10;
+    Console.WriteLine("Последнее число "+num+" это "+lastNum);
+}
